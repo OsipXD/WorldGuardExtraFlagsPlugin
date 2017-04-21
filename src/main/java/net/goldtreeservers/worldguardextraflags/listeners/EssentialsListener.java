ea2a54aadc344eed76d5a9cb172bc6f02990628b
OsipXD <osip.fatkullin@gmail.com>
2017-04-21 22:43:14 +0300

    Simplified EssentialsListener

--- src/main/java/net/goldtreeservers/worldguardextraflags/listeners/EssentialsListener.java
+++ src/main/java/net/goldtreeservers/worldguardextraflags/listeners/EssentialsListener.java
@@ -1,8 +1,10 @@
 package net.goldtreeservers.worldguardextraflags.listeners;
 
+import com.sk89q.worldguard.session.SessionManager;
 import net.ess3.api.events.GodStatusChangeEvent;
 import net.goldtreeservers.worldguardextraflags.WorldGuardExtraFlagsPlugin;
 import net.goldtreeservers.worldguardextraflags.flags.GodmodeFlag;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,8 +12,11 @@ import org.bukkit.event.Listener;
 public class EssentialsListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onGodStatusChangeEvent(GodStatusChangeEvent event) {
-        if (!WorldGuardExtraFlagsPlugin.getWorldGuard().getSessionManager().hasBypass(event.getController().getBase(), event.getController().getBase().getWorld())) {
-            if (WorldGuardExtraFlagsPlugin.getWorldGuard().getSessionManager().get(event.getController().getBase()).getHandler(GodmodeFlag.class).getIsGodModeEnabled() != null) {
+        SessionManager sessionManager = WorldGuardExtraFlagsPlugin.getWorldGuard().getSessionManager();
+        Player player = event.getController().getBase();
+
+        if (!sessionManager.hasBypass(player, player.getWorld())) {
+            if (sessionManager.get(player).getHandler(GodmodeFlag.class).getIsGodModeEnabled() != null) {
                 event.setCancelled(true);
             }
         }
